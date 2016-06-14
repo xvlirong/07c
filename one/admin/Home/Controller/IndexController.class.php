66b005779b91a315fc938f3bf38433ea33aff68c
hqj123 <1196069929@qq.com>
2016-06-14 08:52:30 +0800

    hqj

--- one/admin/Home/Controller/IndexController.class.php
+++ one/admin/Home/Controller/IndexController.class.php
@@ -3,14 +3,11 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $obj=M('index');
-        $data=$obj->sel(1);
-        print_r($data);
-            //$this->display('login');
+       $this->display('login');
     }
     public function login(){
         //echo 123;
-        //$this->display('index');
+        $this->display('index');
 
 
     }
