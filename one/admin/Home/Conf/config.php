d547ec2a9795250152ca3cdbbef558891c703d05
hqj123 <1196069929@qq.com>
2016-06-14 08:51:54 +0800

    hqj

--- one/admin/Home/Conf/config.php
+++ one/admin/Home/Conf/config.php
@@ -1,10 +1,12 @@
 <?php
 return array(
-    'DB_TYPE'               =>  'mysql',     // 数据库类型
-    'DB_HOST'               =>  '127.0.0.1', // 服务器地址
-    'DB_NAME'               =>  'oa',          // 数据库名
-    'DB_USER'               =>  'root',      // 用户名
-    'DB_PWD'                =>  'root',          // 密码
-    'DB_PORT'               =>  '3306',        // 端口
-    'DB_PREFIX'             =>  ''    // 数据库表前缀
+    //'配置项'=>'配置值'
+    'DB_TYPE'   => 'mysql', // 数据库类型
+    'DB_HOST'   => '192.168.1.218', // 服务器地址
+    'DB_NAME'   => 'renrendai', // 数据库名
+    'DB_USER'   => 'root', // 用户名
+    'DB_PWD'    => 'root', // 密码
+    'DB_PORT'   => 3306, // 端口
+    'DB_PREFIX' => 'yi_', // 数据库表前缀
+    'DB_CHARSET'=> 'utf8', // 字符集
 );
